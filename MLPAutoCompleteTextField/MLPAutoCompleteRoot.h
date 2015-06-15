268b6d63907e5931116b3a09b9eb1a90b08c3c16
Jakob Haider <haijak@jakobs-mbp.bluesource.local>
2015-06-15 13:55:32 +0200

    add MLPAutoCompleteRoot that enables auto-completion support in tableview cells

---
+++ MLPAutoCompleteTextField/MLPAutoCompleteRoot.h
@@ -0,0 +1,17 @@
+//
+//  BaseAutoComplete.h
+//  app
+//
+//  Created by Jakob Haider on 15/06/15.
+//  Copyright (c) 2015 bluesource - mobile solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MLPAutoCompleteTextField.h"
+
+@interface MLPAutoCompleteRoot : UIView
+
+@property (nonatomic, weak) MLPAutoCompleteTextField *txtAutoComplete;
+
+@end
---
+++ MLPAutoCompleteTextField/MLPAutoCompleteRoot.m
@@ -0,0 +1,30 @@
+//
+//  BaseAutoComplete.m
+//  app
+//
+//  Created by Jakob Haider on 15/06/15.
+//  Copyright (c) 2015 bluesource - mobile solutions. All rights reserved.
+//
+
+#import "MLPAutoCompleteRoot.h"
+
+@implementation MLPAutoCompleteRoot
+
+-(UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event
+{
+    UITableView* tableView = self.txtAutoComplete.autoCompleteTableView;
+    
+    if(tableView != nil && tableView.superview != nil && !tableView.hidden)
+    {
+        
+        bool contains = CGRectContainsPoint(tableView.frame, point);
+        
+        if(contains) {
+            return tableView;
+        }
+    }
+    
+    return [super hitTest:point withEvent:event];
+}
+
+@end
